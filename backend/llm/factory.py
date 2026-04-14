866d2eb302103d515ac8304ea546eb2d2e53cf3d
Karthik <99889801+KarthikChimkode@users.noreply.github.com>
2026-04-14 19:00:51 +0530

    fixed typos

--- backend/llm/factory.py
+++ backend/llm/factory.py
@@ -12,11 +12,11 @@ class LLMFactory:
     @staticmethod()
     def get_provider() -> LLMProvider:
         # We get the 'LLM Proviedr' from our settings
-        provider_type() = settings.LLM_PROVIDER.lower()
+        provider_type = settings.LLM_PROVIDER.lower()
 
         if provider_type == "gemini":
             return GeminiProvider()
         elif provider_type == "ollama":
             return OllamaProvider()
         else:
-            rasie ValueError(f"Unknow LLM Provider: {provider_type}")
+            raise ValueError(f"Unknow LLM Provider: {provider_type}")
